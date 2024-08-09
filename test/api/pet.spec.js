af0e65bf80ad1f483ac96ff7f794283f993b58ca
paulohqsouza <110414038+paulohqsouza@users.noreply.github.com>
2024-08-08 22:57:01 -0300

    Metódos Post, Get, Put e Delete

--- test/api/pet.spec.js
+++ test/api/pet.spec.js
@@ -10,15 +10,57 @@ describe('API PetStore Swagger - Entidade Pet', () => {
     const request = supertest('https://petstore.swagger.io/v2') // BaseURL
     
     // Funções ou métodos: Its
-    it('POST Pet', ( ) => {
+    it('POST Pet', async ( ) => {
         // Atributos, Campos, Características, Configurações
+        const pet = await require('../../vendors/json/pet.json')
 
-        // Funções de Apoio (Opcional)
-
-        // Funções de Teste em Si
+        //Função de Teste em Si
+        return await request
+            .post('/pet')
+            .send(pet)
+            .then((res) => {
+                expect(res.statusCode).toBe(200)
+                expect(res.body.id).toBe(petId)
+                expect(res.body.name).toBe('Snoopy')
+                expect(res.body.category.name).toBe('dog')
+                expect(res.body.tags[0].name).toBe('vaccined')
+            })
 
 
     }) // Final do método POST
+
+    it('GET Pet', async () => {
+        return await request
+        .get(`/pet/${petId}`)
+        .then((res) => {
+            expect(res.statusCode).toBe(200)
+            expect(res.body.id).toBe(petId)
+            expect(res.body.status).toBe('available')
+        })
+    })
+
+    it('PUT Pet', async () => {
+        const pet = await require('../../vendors/json/petput.json')
+
+        return await request
+            .put('/pet')
+            .send(pet)
+            .then((res) =>{
+                expect(res.statusCode).toEqual(200)
+                expect(res.body.status).toEqual('sold')
+            })
+                
+    })
+
+    it('DELETE Pet', async () => {
+        return await request
+            .delete(`/pet/${petId}`)
+            .then((res) => {
+                expect(res.statusCode).toEqual(200)
+                expect(res.body.code).toEqual(200)
+                expect(res.body.message).toBe(petId.toString())
+            })
+    })
 
     
 })// Termina a describe
